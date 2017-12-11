5a3cb8fb8c3e9e6f4f9747ba48d81df3168c1638
Th3WiseAssassin <34432183+Th3WiseAssassin@users.noreply.github.com>
2017-12-11 00:01:55 -0800

    change client to bot

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
 //bot.login('Mzg5MjQxNDg5MjgyMDM5ODE4.DQ4-3A.3DUBhTxufrncpz5CtaYRTRkGECI'); //for running from pc don't change this
-client.login(process.env.BOT_TOKEN); //for running from heroku
+bot.login(process.env.BOT_TOKEN); //for running from heroku
