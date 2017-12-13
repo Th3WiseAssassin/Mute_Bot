d6aadbdaced871b0633f49b367243db5f8a11e06
Th3WiseAssassin <34432183+Th3WiseAssassin@users.noreply.github.com>
2017-12-12 18:46:18 -0800

    userinfo command
    Created the user info command

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const botSettings = require("./botSettings.json"); //import the bot settings from botSettings.json file
-const prefix = botSettings.prefix;
-const commando = require('discord.js-commando'); //imports the discord.js-commando library
-const bot = new commando.Client(); //creates a new commando client called bot
+const prefix = botSettings.prefix; //sets the prefix from the botSettings.json into a variable called prefix to save keystrokes
+const Discord = require('discord.js'); //imports the discord.js library
+const bot = new Discord.Client(); //creates a new Discord client called bot
 
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
+bot.on("message", async message => {
+    if(message.author.bot) return; //ignore messages from bot accounts
+    if(message.channel.type === "dm") return; //ignore dm's
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return; //if message doesn't start with the prefix ignore the command
+
+    if(command === `${prefix}userinfo`){
+        let embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username)
+            .setDescription("This is the user's info!")
+            .setColor("#008000");
+        
+        message.channel.sendEmbed(embed);
+    }
+});
 
 //bot.login('Mzg5MjQxNDg5MjgyMDM5ODE4.DQ4-3A.3DUBhTxufrncpz5CtaYRTRkGECI'); //for running from pc don't change this
 bot.login(process.env.BOT_TOKEN); //for running from heroku
