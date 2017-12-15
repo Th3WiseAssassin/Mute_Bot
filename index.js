874d3245d5ea02c76b986f2d394aa7510f69a9bf
Th3WiseAssassin <34432183+Th3WiseAssassin@users.noreply.github.com>
2017-12-14 22:22:00 -0800

    updated !mute command

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on("message", async message => {
             .addField("ID", message.author.id) //outputs the users id #
             .addField("Created At", message.author.createdAt); //outputs when the user created their Discord account
 
-        message.channel.sendEmbed(embed); //outputs all the infor save in the embed above
+        message.channel.send(embed); //outputs all the infor save in the embed above
 
         return;
     }
@@ -36,7 +36,7 @@ bot.on("message", async message => {
 /*Make this message sound more like mute---------------------------
                                                                    |
                                                                    V                             */
-         return mesage.channel.sendMessage("You don't have the manage messages permission")//Checks to make sure that the person executing the !mute command has the permission 
+         return message.channel.send("You don't have the manage messages permission.")//Checks to make sure that the person executing the !mute command has the permission 
         }
 
         //Get the mentioned user, return if there is none.
@@ -44,12 +44,47 @@ bot.on("message", async message => {
 /*Make this message sound more like mute---------------------------------
                                                                          |
                                                                          V                             */
-            return message.channel.sendMessage("You did not specify a user mention or ID!"); //If the the command isn't entered correctly say the message here
+            return message.channel.send("You did not specify a user mention or ID!"); //If the the command isn't entered correctly say the message here
         } 
 
-        let toMute = message.mentions.users.first() || message.guild.members.get(args[0]);
+        let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
         
-        //If the mutee has the same or a higher role than the muter, return.
+        //create a role called muted
+        let role = message.guild.roles.find(r => r.name === "Muted");
+        if(!role) {
+            try {
+                role = await message.guild.createRole({
+                    name: "Muted",
+                    color: "#FF0000",
+                    permissions: []
+                });
+    
+                message.guild.channels.forEach(async (channel, id) => {
+                    await channel.overwritePermissions(role, {
+                        ADD_REACTIONS: false,
+                        VIEW_CHANNEL: false,
+                        READ_MESSAGES: false,
+                        SEND_MESSAGES: false,
+                        READ_MESSAGE_HISTORY: false
+                    });
+                });
+    
+            } catch(e) {
+                console.log(e.stack);
+            }
+        }
+/*Make this message sound more like mute---------------------------------------
+                                                                               |
+                                                                               V                             */
+        if(toMute.roles.has(role.id)) return message.channel.send("This user is already muted!");
+
+        await(toMute.addRole(role));
+/*Make this message sound more like mute---
+                                           |
+                                           V                             */
+        message.channel.send("I have muted them.");
+
+        return;
     }
 });
 
