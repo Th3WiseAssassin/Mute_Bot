6cfdc2be7b68952994dde532062f9f1fcfbe2e7b
Th3WiseAssassin <34432183+Th3WiseAssassin@users.noreply.github.com>
2017-12-15 13:20:54 -0800

    added !unmute command

--- index.js
+++ index.js
@@ -86,6 +86,44 @@ bot.on("message", async message => {
 
         return;
     }
+
+    //!unmute @<username> || !unmute <user id> command
+    if(command === `${prefix}unmute`) {
+
+        //Check if the command executor has the right permission to do this command.        
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+/*Make this message sound more like mute---------------------------
+                                                                   |
+                                                                   V                             */
+         return message.channel.send("You don't have the manage messages permission.")//Checks to make sure that the person executing the !mute command has the permission 
+        }
+
+        //Get the mentioned user, return if there is none.
+        if (!message.mentions.users.first() && !message.guild.members.get(args[0])) {
+/*Make this message sound more like mute---------------------------------
+                                                                         |
+                                                                         V                             */
+            return message.channel.send("You did not specify a user mention or ID!"); //If the the command isn't entered correctly say the message here
+        } 
+
+        let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+        
+        //create a role called muted
+        let role = message.guild.roles.find(r => r.name === "Muted");
+       
+ /*Make this message sound more like mute-----------------------------------------------
+                                                                                        |
+                                                                                        V                             */
+        if(!role || !toMute.roles.has(role.id)) return message.channel.send("This user is not muted!");
+
+        await(toMute.removeRole(role));
+/*Make this message sound more like mute---
+                                           |
+                                           V                             */
+        message.channel.send("I have unmuted them.");
+
+        return;
+    }
 });
 
 //bot.login('<Inset Bot Token Here>'); //for running from pc don't change this
