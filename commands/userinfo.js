d0e785f351fb5c5ac8e6f36d65a26fa403c7e759
Th3WiseAssassin <34432183+Th3WiseAssassin@users.noreply.github.com>
2017-12-15 18:30:30 -0800

    !userinfo update #5
    added the users avatar to the !userinfo command

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,14 +2,14 @@ const Discord = module.require("discord.js"); //imports the discord.js library
 
 module.exports.run = async (bot, message,args) => {
     let embed = new Discord.RichEmbed()
-            .setAuthor(message.author.username)
+            .setAuthor(message.author.username, message.author.displayAvatarURL)
             .setDescription("This is the user's info!")
             .setColor("#008000") //sets the color of the side bar
             .addField("Full Username", message.author.username + "#" + message.author.discriminator) //outputs the users full name including there name and discriminator (ex: Stephen#9557)
             .addField("ID", message.author.id) //outputs the users id #
             .addField("Created At", message.author.createdAt) //outputs when the user created their Discord account
-            .addField("Avatar","")
-            .setImage(message.author.displayAvatarURL);
+            .addField("Avatar", message.author.displayAvatarURL)    
+            .setImage(message.author.displayAvatarURL);       
 
         message.channel.send(embed); //outputs all the infor save in the embed above
 }
